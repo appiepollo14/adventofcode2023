d81d09729d61005fb68b7c477b77311b12e3a805
asjervanasten <asjer94@live.com>
2023-12-03 17:00:20 +0100

    Day 3 incomplete

---
+++ src/main/java/nl/avasten/Day3.java
@@ -0,0 +1,236 @@
+package nl.avasten;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+@Path("/day3")
+public class Day3 {
+
+    int sum = 0;
+    static final int lineSize = 140;
+    static Map<Integer, List<Integer>> symbolsPerLine = new HashMap<>();
+    static Map<Integer, String> mappedInput = inputToMap();
+    static Map<Integer, Map<Integer, List<Integer>>> numbersPerLine = new HashMap<>();
+
+    @GET
+    @Path("/test")
+    public String testInput() {
+
+        //Map symbol Positions
+        for (int i = 0; i < mappedInput.size(); i++) {
+            List<Integer> symbolList = getSymbolPositions(mappedInput.get(i));
+            symbolsPerLine.put(i, symbolList);
+        }
+
+        // Get numbers per line,
+        for (int i = 0; i < mappedInput.size(); i++) {
+            numbersPerLine.put(i, getNumberPositionMap(mappedInput.get(i)));
+        }
+
+        // check adjecening fields for each number, if found, add to sum
+        for (int i = 0; i < numbersPerLine.size(); i++) {
+            Map<Integer, List<Integer>> numbersAndPosition = numbersPerLine.get(i);
+            // First row
+            if (i == 0) {
+                // Loop through each number
+                for (Integer k : numbersAndPosition.keySet()) {
+                    List<Integer> positions = numbersAndPosition.get(k);
+
+                    if (checkSameLine(i, positions) || checkLineBelow(i, positions)) {
+                        System.out.println("Line: " + i + " , number: " + k + " , hit: true");
+                        sum += k;
+                    } else {
+                        System.out.println("Line: " + i + " , number: " + k + " , hit: false");
+                    }
+                }
+
+                // Last row
+            } else if (i == numbersPerLine.size() - 1) {
+                for (Integer k : numbersAndPosition.keySet()) {
+                    List<Integer> positions = numbersAndPosition.get(k);
+
+                    if (checkSameLine(i, positions) || checkLineAbove(i, positions)) {
+                        System.out.println("Line: " + i + " , number: " + k + " , hit: true");
+                        sum += k;
+                    } else {
+                        System.out.println("Line: " + i + " , number: " + k + " , hit: false");
+                    }
+                }
+                // All other rows
+            } else {
+                for (Integer k : numbersAndPosition.keySet()) {
+                    List<Integer> positions = numbersAndPosition.get(k);
+
+                    if (checkSameLine(i, positions) || checkLineBelow(i, positions) || checkLineAbove(i, positions)) {
+                        System.out.println("Line: " + i + " , number: " + k + " , hit: true");
+                        sum += k;
+                    } else {
+                        System.out.println("Line: " + i + " , number: " + k + " , hit: false");
+                    }
+                }
+            }
+        }
+
+        return "Sum: " + sum;
+
+    }
+
+    private static Map<Integer, String> inputToMap() {
+
+        Map<Integer, String> mappedInput = new HashMap<>();
+        int lineNumber = 0;
+
+        try {
+            Scanner scanner = new Scanner(new File("src/main/resources/puzzle3.txt"));
+
+            while (scanner.hasNextLine()) {
+                String currentLine = scanner.nextLine();
+                currentLine = currentLine.replaceAll("\\.", " ");
+                mappedInput.put(lineNumber, currentLine);
+                lineNumber++;
+            }
+
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return mappedInput;
+    }
+
+    private static List<Integer> getSymbolPositions(String currentLine) {
+        List<Integer> symbolPositions = new ArrayList<>();
+
+        for (int i = 0; i < currentLine.length(); i++) {
+            if (isSymbol(currentLine.charAt(i))) {
+                symbolPositions.add(i);
+            }
+
+            //System.out.println("Char : " + currentLine.charAt(i) + " , " + isSymbol(currentLine.charAt(i)));
+
+        }
+        return symbolPositions;
+
+    }
+
+    private static boolean checkSameLine(int lineNumber, List<Integer> positions) {
+        // Get left from number:
+        int left = Collections.min(positions) - 1;
+        if (left > 0) {
+            return symbolsPerLine.get(lineNumber).contains(left);
+        }
+
+        int right = Collections.max(positions) + 1;
+        if (right < lineSize) {
+            return symbolsPerLine.get(lineNumber).contains(right);
+        }
+
+        return false;
+    }
+
+    private static boolean checkLineBelow(int lineNumber, List<Integer> positions) {
+
+        int lineNumberBelow = lineNumber + 1;
+
+        // Get leftUnder from number:
+        int left = Collections.min(positions) - 1;
+        if (left > 0) {
+            if (symbolsPerLine.get(lineNumberBelow).contains(left)) {
+                return true;
+            }
+        }
+
+        // Get rightUnder
+        int right = Collections.max(positions) + 1;
+        if (right < lineSize) {
+            if (symbolsPerLine.get(lineNumberBelow).contains(right)) {
+                return true;
+            }
+        }
+
+        //All below
+        boolean found = false;
+        for (int pos : positions) {
+            if (symbolsPerLine.get(lineNumberBelow).contains(pos)) {
+                found = true;
+                break;
+            }
+        }
+
+        return found;
+    }
+
+    private static boolean checkLineAbove(int lineNumber, List<Integer> positions) {
+
+        int lineNumberAbove = lineNumber - 1;
+
+        // Get leftAbove from number:
+        int left = Collections.min(positions) - 1;
+        if (left > 0) {
+            if (symbolsPerLine.get(lineNumberAbove).contains(left)) {
+                return true;
+            }
+        }
+
+        // Get rightAbove
+        int right = Collections.max(positions) + 1;
+        if (right < lineSize) {
+            if (symbolsPerLine.get(lineNumberAbove).contains(right)) {
+                return true;
+            }
+        }
+
+        //All above
+        boolean found = false;
+        for (int pos : positions) {
+            if (symbolsPerLine.get(lineNumberAbove).contains(pos)) {
+                found = true;
+                break;
+            }
+        }
+
+        return found;
+    }
+
+    private static boolean isSymbol(char ch) {
+        return !Character.isLetter(ch) && !Character.isDigit(ch) && !Character.isSpaceChar(ch);
+    }
+
+    private static boolean isNumber(char ch) {
+        return Character.isDigit(ch);
+    }
+
+
+    private static Map<Integer, List<Integer>> getNumberPositionMap(String currentLine) {
+        Map<Integer, List<Integer>> numberPositionMap = new HashMap<>();
+
+        StringBuilder number = new StringBuilder();
+        List<Integer> positions = new ArrayList<>();
+        for (int i = 0; i < currentLine.length(); i++) {
+
+            if (isNumber(currentLine.charAt(i))) {
+                number.append(currentLine.charAt(i));
+                positions.add(i);
+            } else {
+                if (!number.isEmpty()) {
+                    numberPositionMap.put(Integer.parseInt(number.toString()), positions);
+                    number = new StringBuilder();
+                    positions = new ArrayList<>();
+                }
+            }
+
+        }
+        if (!number.isEmpty()) {
+            numberPositionMap.put(Integer.parseInt(number.toString()), positions);
+        }
+//        System.out.println(numberPositionMap);
+
+        return numberPositionMap;
+    }
+
+
+}
