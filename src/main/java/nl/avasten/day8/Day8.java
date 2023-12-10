d1ffedfa21f5273f193679ca1f8cdc5dba194831
asjervanasten <asjer94@live.com>
2023-12-10 21:03:55 +0100

    Day 8 puzzle B tried

--- src/main/java/nl/avasten/day8/Day8.java
+++ src/main/java/nl/avasten/day8/Day8.java
@@ -1,5 +1,6 @@
 package nl.avasten.day8;
 
+import io.quarkus.runtime.Startup;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -15,10 +16,19 @@ public class Day8 {
     static String instruction;
     static Map<String, Node> nodes = new HashMap<>();
 
+    static {
+        mapInput();
+    }
+
+    @Startup
+    public void run() {
+        System.out.println("Starting:");
+        System.out.println(calculateGhostSteps());
+    }
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String calculateSteps() {
-        mapInput();
 
         int steps = 0;
         Node currentNode = getNode("AAA");
@@ -35,7 +45,6 @@ public class Day8 {
 
                 steps++;
                 if (Objects.equals(currentNode.getNodeName(), "ZZZ")) {
-                    System.out.println("FOUND!");
                     break outerLoop;
                 }
             }
@@ -44,8 +53,58 @@ public class Day8 {
         return "Steps: " + steps + " currentNode: " + currentNode.getNodeName();
     }
 
+    @GET
+    @Path("/b")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String calculateGhostSteps() {
+
+        long steps = 0;
+        List<Node> currentNodes = getNodesEndingWithA();
+
+        outerLoop:
+        while (true) {
+            for (char c : instruction.toCharArray()) {
+                List<Node> temp = new ArrayList<>();
+                for (Node n : currentNodes) {
+                    if (c == 'L') {
+                        temp.add(getNode(n.getLeftNode()));
+                    } else {
+                        temp.add(getNode(n.getRightNode()));
+                    }
+                }
+
+                currentNodes = temp;
+                steps++;
+
+                int check = 0;
+                for (Node n: currentNodes) {
+                    if (n.getNodeName().endsWith("Z")) {
+//                        System.out.println("Klopt!");
+                        check++;
+                    }
+                }
+
+//                System.out.println("Check: " + check);
+                if (check == 6) {
+                    break outerLoop;
+                }
+
+//                System.out.println("CurrentNodes: " + currentNodes + " , steps: " + steps);
+            }
+        }
+
+        return "Steps: " + steps + " currentNode: " + currentNodes;
+    }
+
     public Node getNode(String n) {
         return nodes.get(n);
+    }
+
+    public List<Node> getNodesEndingWithA() {
+        String suffix = "A";
+        return nodes.entrySet().stream()
+                .filter(entry -> entry.getKey().endsWith(suffix)).map(Map.Entry::getValue)
+                .toList();
     }
 
     public static void mapInput() {
@@ -76,7 +135,7 @@ public class Day8 {
                 String[] s = currentLine.substring(i + 1).split(",");
 //                System.out.println(currentLine + " CurNode: " + curNode + " split: " + s[0] + " , " + s[1]);
 
-                nodes.put(curNode, new Node(curNode, s[0],s[1]));
+                nodes.put(curNode, new Node(curNode, s[0], s[1]));
                 lineNum++;
             }
 
