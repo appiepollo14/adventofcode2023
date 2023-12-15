248eec6300c1507f6dcb29ae373f1eed5df0c3e0
asjervanasten <asjer94@live.com>
2023-12-15 23:01:19 +0100

    Start day 10

--- src/main/java/nl/avasten/day10/Day10.java
+++ src/main/java/nl/avasten/day10/Day10.java
@@ -7,98 +7,80 @@ import jakarta.ws.rs.core.MediaType;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 @Path("/day10")
 public class Day10 {
 
+    static int startPuntRij;
+    static int startPuntKolom;
+    static Map<Integer, Map<Integer, Character>> mappedInput = new HashMap<>();
+
+    static {
+        mapInput();
+    }
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String calculateSteps() {
 
-        long sum = 0;
-        List<List<Integer>> sequencelist = mapInput();
+        int steps = 0;
+        System.out.println("Startpunt rij: " + startPuntRij + ", kolom: " + startPuntKolom);
+        System.out.println("Mapped input: " + mappedInput);
 
-        for (List<Integer> l : sequencelist) {
-            System.out.println("Original list: " + l);
-            long nextval = l.getLast() + calcAdditional(calcDif(l));
-            sum += nextval;
-        }
+        determineStartKind();
 
-        return "Sequences: " + sum;
+        return "Sequences: " + steps;
     }
 
-    public static long calcPrevious(List<List<Integer>> input) {
-        long lastStep = input.getLast().getFirst();
-        System.out.println("Last line first pos: " + lastStep);
-        for (int i = input.size() - 2; i >= 0; i--) {
-            lastStep = input.get(i).getFirst() - lastStep;
-        }
-        return lastStep;
+    public static String determineStartKind() {
+        //Naar boven?
+        char b = mappedInput.get(startPuntRij - 1).get(startPuntKolom);
+        boolean boven = (b == '|' || b == '7' || b == 'F');
+        //Naar links?
+        char l = mappedInput.get(startPuntRij).get(startPuntKolom - 1);
+        boolean links = (l == '-' || l == 'L' || l == 'F');
+        //Naar onder?
+        char o = mappedInput.get(startPuntRij + 1).get(startPuntKolom);
+        boolean onder = (o == '|' || o == 'L' || o == 'J');
+        //Naar rechts?
+        char r = mappedInput.get(startPuntRij).get(startPuntKolom + 1);
+        boolean rechts = (l == '-' || l == 'J' || l == '7');
+
+        System.out.println("Boven: " + boven + " , onder: " + onder + " , links: " + links + " , rechts: " + rechts);
+        return new String();
     }
 
-    public static long calcAdditional(List<List<Integer>> input) {
-        System.out.println("Input: " + input);
-        long lastStep = input.getLast().getLast();
-        for (int i = input.size() - 2; i >= 0; i--) {
-            System.out.println("Next val: " + (input.get(i).getLast() + lastStep));
-            lastStep = input.get(i).getLast() + lastStep;
-        }
-        return lastStep;
+    public static boolean goLeft(char input) {
+        return false;
     }
 
-    public static List<List<Integer>> calcDif(List<Integer> input) {
-
-        List<List<Integer>> diffList = new ArrayList<>();
-        List<Integer> diff = new ArrayList<>();
-        if (input.size() <= 1) {
-            System.out.println("TOO SMALL");
-        } else {
-            for (int i = 1; i < input.size(); i++) {
-                int cur = input.get(i);
-                int prev = input.get(i - 1);
-                diff.add(cur - prev);
-            }
-        }
-
-        if (!isAllZeros(diff)) {
-            diffList.add(diff);
-            diffList.addAll(calcDif(diff));
-        }
-
-        return diffList;
-    }
-
-    public static boolean isAllZeros(List<Integer> list) {
-        for (int value : list) {
-            if (value != 0) {
-                return false; // If any element is not zero, return false
-            }
-        }
-        return true; // All elements are zero
-    }
-
-    public static List<List<Integer>> mapInput() {
-        List<List<Integer>> sequences = new ArrayList<>();
+    public static void mapInput() {
 
         try {
-            Scanner scanner = new Scanner(new File("src/main/resources/puzzle9.txt"));
+            Scanner scanner = new Scanner(new File("src/main/resources/puzzle10.txt"));
 
+            int lineNum = 0;
             while (scanner.hasNextLine()) {
+                Map<Integer, Character> mappedLine = new HashMap<>();
                 String currentLine = scanner.nextLine();
-                String[] s = currentLine.split(" ");
-                sequences.add(Arrays.stream(s).map(Integer::valueOf).toList());
+                for (int i = 0; i < currentLine.length(); i++) {
+                    mappedLine.put(i, currentLine.charAt(i));
+                    if (currentLine.charAt(i) == 'S') {
+                        startPuntRij = lineNum;
+                        startPuntKolom = i;
+                    }
+                }
+                mappedInput.put(lineNum, mappedLine);
+                lineNum++;
             }
 
             scanner.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
-        return sequences;
 
     }
 }
