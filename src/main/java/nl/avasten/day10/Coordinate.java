43c8959211312f73c774129ea2aa31974c2cbf6c
asjervanasten <asjer94@live.com>
2023-12-16 11:49:40 +0100

    Refactor day 10a

---
+++ src/main/java/nl/avasten/day10/Coordinate.java
@@ -0,0 +1,4 @@
+package nl.avasten.day10;
+
+public record Coordinate(int row, int column, char chr) {
+}
--- src/main/java/nl/avasten/day10/Day10.java
+++ src/main/java/nl/avasten/day10/Day10.java
@@ -9,6 +9,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Scanner;
 
 @Path("/day10")
@@ -16,7 +17,10 @@ public class Day10 {
 
     static int startPuntRij;
     static int startPuntKolom;
+    static int steps = 0;
+    static Coordinate startCoordinate;
     static Map<Integer, Map<Integer, Character>> mappedInput = new HashMap<>();
+    static Map<Integer, Map<Integer, Coordinate>> coordinates = new HashMap<>();
 
     static {
         mapInput();
@@ -26,35 +30,65 @@ public class Day10 {
     @Produces(MediaType.TEXT_PLAIN)
     public String calculateSteps() {
 
-        int steps = 0;
         System.out.println("Startpunt rij: " + startPuntRij + ", kolom: " + startPuntKolom);
         System.out.println("Mapped input: " + mappedInput);
 
-        determineStartKind();
+        determineNextStep(startCoordinate);
 
         return "Sequences: " + steps;
     }
 
-    public static String determineStartKind() {
-        //Naar boven?
-        char b = mappedInput.get(startPuntRij - 1).get(startPuntKolom);
-        boolean boven = (b == '|' || b == '7' || b == 'F');
-        //Naar links?
-        char l = mappedInput.get(startPuntRij).get(startPuntKolom - 1);
-        boolean links = (l == '-' || l == 'L' || l == 'F');
-        //Naar onder?
-        char o = mappedInput.get(startPuntRij + 1).get(startPuntKolom);
-        boolean onder = (o == '|' || o == 'L' || o == 'J');
-        //Naar rechts?
-        char r = mappedInput.get(startPuntRij).get(startPuntKolom + 1);
-        boolean rechts = (l == '-' || l == 'J' || l == '7');
-
-        System.out.println("Boven: " + boven + " , onder: " + onder + " , links: " + links + " , rechts: " + rechts);
-        return new String();
+    public static void determineNextStep(Coordinate c) {
+        steps++;
+        if (goLeft(c).isPresent()) {
+            determineNextStep(goLeft(c).get());
+        } else if (goRight(c).isPresent()) {
+            determineNextStep(goRight(c).get());
+        } else if (goUp(c).isPresent()) {
+            determineNextStep(goUp(c).get());
+        } else if (goDown(c).isPresent()){
+            determineNextStep(goDown(c).get());
+        }
     }
 
-    public static boolean goLeft(char input) {
-        return false;
+    public static Optional<Coordinate> goLeft(Coordinate c) {
+        Coordinate n = coordinates.get(c.row()).get(c.column() - 1);
+        char nc = n.chr();
+        if (nc == '-' || nc == 'L' || nc == 'F') {
+            return Optional.of(n);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public static Optional<Coordinate> goRight(Coordinate c) {
+        Coordinate n = coordinates.get(c.row()).get(c.column() + 1);
+        char nc = n.chr();
+        if (nc == '-' || nc == 'J' || nc == '7') {
+            return Optional.of(n);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public static Optional<Coordinate> goUp(Coordinate c) {
+        Coordinate n = coordinates.get(c.row() - 1).get(c.column());
+        char nc = n.chr();
+        if (nc == '|' || nc == 'F' || nc == '7') {
+            return Optional.of(n);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public static Optional<Coordinate> goDown(Coordinate c) {
+        Coordinate n = coordinates.get(c.row() + 1).get(c.column());
+        char nc = n.chr();
+        if (nc == '|' || nc == 'L' || nc == 'J') {
+            return Optional.of(n);
+        } else {
+            return Optional.empty();
+        }
     }
 
     public static void mapInput() {
@@ -65,15 +99,19 @@ public class Day10 {
             int lineNum = 0;
             while (scanner.hasNextLine()) {
                 Map<Integer, Character> mappedLine = new HashMap<>();
+                Map<Integer, Coordinate> coordinateLine = new HashMap<>();
                 String currentLine = scanner.nextLine();
                 for (int i = 0; i < currentLine.length(); i++) {
                     mappedLine.put(i, currentLine.charAt(i));
+                    coordinateLine.put(i, new Coordinate(lineNum, i, currentLine.charAt(i)));
                     if (currentLine.charAt(i) == 'S') {
                         startPuntRij = lineNum;
                         startPuntKolom = i;
+                        startCoordinate = new Coordinate(lineNum, i, 'S');
                     }
                 }
                 mappedInput.put(lineNum, mappedLine);
+                coordinates.put(lineNum, coordinateLine);
                 lineNum++;
             }
 
